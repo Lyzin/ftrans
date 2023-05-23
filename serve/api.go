8702441ea8f857c7665524bea33442b13b345818
lyzin <lyzin@xx.com>
2023-05-23 19:11:36 +0800

    add:增加log

--- serve/api.go
+++ serve/api.go
@@ -9,29 +9,33 @@ package serve
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"path/filepath"
 )
 
-type FileServe struct {}
+type FileServe struct{}
 
 var currentPath = GetFilePathCurrent()
 
-func (f FileServe)FileList(c *gin.Context) {
+func (f FileServe) FileList(c *gin.Context) {
+	log.Printf("currentPath:%v\n", currentPath)
 	c.File(currentPath)
 }
 
 func (f FileServe) FileDownLoadFile(c *gin.Context) {
 	file := c.Param("file")
-	
+	log.Printf("file:%v\n", file)
+
 	action := c.Param("action")
-	
+	log.Printf("action:%v\n", action)
+
 	// 下载链接路径
 	newDownLoadPath := currentPath + "/" + file + action
-	
+
 	if !ObjIsDir(newDownLoadPath) {
 		downLoadFileName := filepath.Base(newDownLoadPath)
 		c.Writer.Header().Add("Content-Type", "application/octet-stream")
-		c.Writer.Header().Add("Content-Disposition", "attachment; filename=" + downLoadFileName)
+		c.Writer.Header().Add("Content-Disposition", "attachment; filename="+downLoadFileName)
 	}
 	c.File(newDownLoadPath)
 }
