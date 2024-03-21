b65b6b92617ac389f93f4ae863c56ba3fd2b5a41
lyzin <lyzin@lyzindeMacBook-Pro.local>
2024-03-21 23:25:51 +0800

    add:添加eTag

--- serve/router.go
+++ serve/router.go
@@ -7,7 +7,12 @@
 
 package serve
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"time"
+)
 
 func InitRouter() *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
@@ -16,7 +21,18 @@ func InitRouter() *gin.Engine {
 
 	// 添加中间件
 	r.Use(func(c *gin.Context) {
+		// 设置eTag
+		eTag := strconv.FormatInt(time.Now().UnixMilli(), 10)
+		// 检查If-None-Match
+		match := c.GetHeader("If-None-Match")
+		if match == eTag {
+			c.Header("ETag", eTag)
+			c.Status(http.StatusNotModified)
+			return
+		}
 		c.Header("Cache-Control", "no-cache")
+		c.Header("ETag", eTag)
+		c.Next()
 	})
 
 	r.GET("/", FileServeHandler.FileList)
