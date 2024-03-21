9995a25568e44a5352bbb4e6d2d5549dea7bad52
lyzin <lyzin@lyzindeMacBook-Pro.local>
2024-03-22 00:28:30 +0800

    add:version1.1.4

--- cmd/version.go
+++ cmd/version.go
@@ -16,7 +16,7 @@ var versionCmd = &cobra.Command{
 	Short: "版本信息",
 	Long:  `查看版本信息`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ftrans@v1.1.3 by liuyang")
+		fmt.Println("ftrans@v1.1.4 by liuyang")
 	},
 }
 
